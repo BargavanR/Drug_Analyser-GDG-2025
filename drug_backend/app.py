ae0f1da8378f4bb52b0a793c310a7db95f65e96c
Bargavan R <bargavanroboticsengineer@gmail.com>
2025-04-06 19:44:32 +0530

    backend 4

--- drug_backend/app.py
+++ drug_backend/app.py
@@ -3,10 +3,18 @@ from rdkit import Chem
 from rdkit.Chem import Descriptors
 import joblib
 import json
+import os
 
-# Load pre-trained model and scaler
-final_model = joblib.load(r"drug_backend\assets\ML_models\drug_toxicity_model.pkl")
-scaler = joblib.load(r"drug_backend\assets\ML_models\scaler.pkl")
+# Get the absolute path to the directory where this file (app.py) is located
+base_path = os.path.dirname(os.path.abspath(__file__))
+
+# Join path parts using os.path.join to ensure cross-platform compatibility
+model_path = os.path.join(base_path, "assets", "ML_models", "drug_toxicity_model.pkl")
+scaler_path = os.path.join(base_path, "assets", "ML_models", "scaler.pkl")
+
+# Load the model and scaler
+final_model = joblib.load(model_path)
+scaler = joblib.load(scaler_path)
 
 app = Flask(__name__)
 
